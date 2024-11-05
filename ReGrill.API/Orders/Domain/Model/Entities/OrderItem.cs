e5ee6125ba9cc2e298e6433eec77892a4cf8fcab
Jamir <u201824409@upc.edu.pe>
2024-11-05 18:02:21 -0500

    feat(orders): Added OderItem Entity.

---
+++ ReGrill.API/Orders/Domain/Model/Entities/OrderItem.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+using Regrill.API.Orders.Domain.Model.Aggregates;
+
+namespace Regrill.API.Orders.Domain.Model.Entities;
+
+public class OrderItem
+{
+    public Guid OrderItemId { get; private set; } 
+    public Guid OrderId { get; private set; } 
+    public string ProductName { get; private set; }
+    public int Quantity { get; private set; }
+    public decimal Price { get; private set; }
+    
+    [JsonIgnore]
+    public Order? Order { get;  }
+
+    public OrderItem(Guid orderId, string productName, int quantity, decimal price)
+    {
+        OrderItemId = Guid.NewGuid();
+        OrderId = orderId;
+        ProductName = productName;
+        Quantity = quantity;
+        Price = price;
+    }
+}
